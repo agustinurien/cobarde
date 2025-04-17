d086ad8bad3d06d8ff45dd80d4b5b2c5a055d9cf
Agustin Urien <agus.urien2@gmail.com>
2025-04-17 15:44:23 -0300

    add vercel adapter for MercadoPago compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercelAdapter from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercelAdapter(),
   integrations: [react()],
+
 });
