97aa64fc4f866df36eb2c8eb1c70c7da070319c0
MacBookPro <macbookpro@MacBook-Pro-de-MacBookPro.local>
2025-04-29 15:24:11 -0300

    cambiamos precios

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -16,7 +16,7 @@ export async function POST({ request }) {
 
     const data = {
       from: "Cobarde Web <onboarding@resend.dev>",
-      to: "agus.urien2@gmail.com",
+      to: "cobardeoficial@gmail.com",
       subject: `Nuevo mensaje de ${name} - ${matter}`,
       text: ` Has recibido un nuevo mensaje de contacto:
 Nombre: ${name}
