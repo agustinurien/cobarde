f7f20c3d7f5d89de6d030aaa70b66acdaa5fbbe8
MacBookPro <macbookpro@MacBook-Pro-de-MacBookPro.local>
2025-04-29 11:56:38 -0300

    iconlogo

--- src/components/botonMercadoPago/Boton.jsx
+++ src/components/botonMercadoPago/Boton.jsx
@@ -1,44 +1,43 @@
-import React from 'react'
-import { MdArrowOutward } from 'react-icons/md';
-import './boton.css'
+import React from "react";
+import { MdArrowOutward } from "react-icons/md";
+import "./boton.css";
 
 const Boton = (plato) => {
-    const crearPreferenciaCompra = async () => {
-        try {
-            const respuesta = await fetch('/api/crear-preferencia', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    title: plato.plato.titulo,
-                    quantity: 1,
-                    unit_price: plato.plato.precio,
-                }),
-            });
+  const crearPreferenciaCompra = async () => {
+    try {
+      const respuesta = await fetch("/api/crear-preferencia", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: plato.plato.titulo,
+          quantity: 1,
+          unit_price: plato.plato.precio,
+        }),
+      });
 
-            const data = await respuesta.json();
+      const data = await respuesta.json();
 
+      if (data.init_point) {
+        // Redirigir al usuario a la página de pago de Mercado Pago
+        window.location.href = data.init_point;
+      } else {
+        // Manejo de errores
+        alert("Hubo un error al crear la preferencia de pago");
+      }
+    } catch (error) {
+      console.error("Error en la creación de la preferencia:", error);
+    }
+  };
+  return (
+    <div className="buttonsMercadoPago">
+      <button onClick={crearPreferenciaCompra}>Pagar con Mercado Pago</button>
+      <button onClick={crearPreferenciaCompra} className="arrow">
+        <MdArrowOutward />
+      </button>
+    </div>
+  );
+};
 
-            if (data.init_point) {
-                // Redirigir al usuario a la página de pago de Mercado Pago
-                window.location.href = data.init_point;
-            } else {
-                // Manejo de errores
-                alert('Hubo un error al crear la preferencia de pago');
-            }
-        } catch (error) {
-            console.error('Error en la creación de la preferencia:', error);
-
-        }
-    };
-    return (
-        <div className="buttons">
-            <button onClick={crearPreferenciaCompra}>Pagar con Mercado Pago</button>
-            <button onClick={crearPreferenciaCompra} className="arrow"><MdArrowOutward /></button>
-        </div>
-
-    )
-}
-
-export default Boton
+export default Boton;
